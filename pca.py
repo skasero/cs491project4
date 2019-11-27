2b623505baabda73e6466809a845f5b9ef7e6546
skasero <spencerkkr@gmail.com>
2019-11-26 16:45:58 -0800

    Everything before pro before project_data

--- pca.py
+++ pca.py
@@ -1,13 +1,19 @@
 import numpy as np
 
 def compute_Z(X, centering=True, scaling=False):
-    pass
+    if(centering):
+        mean = np.mean(X,axis=0)
+        X = np.subtract(X,mean)
+    if(scaling):
+        std = np.std(X,axis=0)
+        X = np.divide(X,std)
+    return X
 
 def compute_covariance_matrix(Z):
-    pass
+    return np.matmul(Z.transpose(),Z)
 
 def find_pcs(COV):
-    pass
+    return np.linalg.eig(COV)
 
 def project_data(Z, PCS, L, k, var):
     pass
