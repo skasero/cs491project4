e8966190d84ea68d24df6a787a4009ac37b8e9c7
Egan Sutherland <egans@nevada.unr.edu>
2019-12-01 16:21:09 -0800

    added line to create directory if it doesn't exist and moved save line from driver to compress, as described in compress_images specs

--- compress.py
+++ compress.py
@@ -10,6 +10,7 @@ def compress_images(DATA,k):
     L, U = pca.find_pcs(COV)
     Z_star = pca.project_data(Z,U,L,k,0)
     X_compressed = np.dot(Z_star,U[:,:k].T)
+    save_data(X_compressed)
     return X_compressed
 
 def load_data(input_dir):
@@ -19,8 +20,10 @@ def load_data(input_dir):
         output_array.append(tmp_array.flatten())
     return np.array(output_array).T
 
-def save_data(X_compressed, output_dir = 'Data/myoutput/'):
+def save_data(X_compressed, output_dir = 'Data/Output/'):
     count = 0
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     for images in X_compressed.T:
         images *= 255.0/images.max()
         plt.imsave(output_dir + 'output_' + str(count) + '.png',np.reshape(images,(-1,48)),cmap='gray')
